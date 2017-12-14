73b16f84d25c73401a056e26ebbd3504ecb3c272
Patrick Ditzel <p.ditzel@micromata.de>
2017-12-14 13:16:27 +0100

    As this program grwos and grows it was time do make an own repository for it an move it into.

---
+++ INSTALL.md
@@ -0,0 +1,13 @@
+# Installation of the script
+
+1.) Run the install.sh - script
+
+```bash
+./install.sh
+```
+
+2.) Edit the configfile that it fits to your environment.
+
+3.) Make changes to the skript and some pull requests ;-).
+
+
---
+++ install.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+# ToDo
+#
+# 1. Set installationpath (e.g. /usr/ or /usr/local/) => INSTPATH
+# 2. Generate directories
+#	- $INSTPATH/lib/dbb
+# 3. Check if it is an local user installation odr a global system installation
+# 4. If it is a global installation mkdit /etc/dbb.cfg.d an cop dbb.cf to /etc
+#	 If it is an local user installation create ~/.dbb.cfg.d an cop dbb.cfg to ~/.ddb.cfg
+# 5. Copy dbb to $INSTALLPATH/bin/dbb and create symlink from $INSTALLPATH/bin/dbb to $INSTALLPATH/bin/dbbi
