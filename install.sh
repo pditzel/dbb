2afb6c065d69d5b3fe00d64930b121434a48da6a
Patrick Ditzel <patrick@central-computer.de>
2017-12-15 22:04:20 +0100

    Moved databasefunction into database specific modules and also the configuration. Still work in progress. Installer does noct work yet

--- install.sh
+++ install.sh
@@ -9,3 +9,5 @@
 # 4. If it is a global installation mkdit /etc/dbb.cfg.d an cop dbb.cf to /etc
 #	 If it is an local user installation create ~/.dbb.cfg.d an cop dbb.cfg to ~/.ddb.cfg
 # 5. Copy dbb to $INSTALLPATH/bin/dbb and create symlink from $INSTALLPATH/bin/dbb to $INSTALLPATH/bin/dbbi
+
+# To configure dbb during installation, create some dialogs and ask some questions
